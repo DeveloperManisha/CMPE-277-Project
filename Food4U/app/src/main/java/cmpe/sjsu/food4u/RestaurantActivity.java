412e8fc7d2a02d007b95d3e9af276bb417ef1fbd
Manisha Shivshette <Manisha.Shivshette@sjsu.edu>
2018-05-08 21:47:00 -0700

    code cleanup

--- Food4U/app/src/main/java/cmpe/sjsu/food4u/RestaurantActivity.java
+++ Food4U/app/src/main/java/cmpe/sjsu/food4u/RestaurantActivity.java
@@ -83,35 +83,6 @@ public class RestaurantActivity extends AppCompatActivity
             System.out.println("***********Database is connected");
         }
         fetchAndDisplayMenuItems();
-       /* dbReference.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s){
-
-                FoodItem value = dataSnapshot.getValue(FoodItem.class);
-                System.out.println("Database is connected:"+value.getPicture());
-
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });*/
 
     }
     View.OnClickListener floatingButtonListener = new View.OnClickListener() {
@@ -162,7 +133,7 @@ public class RestaurantActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -250,57 +221,6 @@ public class RestaurantActivity extends AppCompatActivity
 
                 return new MenuViewHolder(view);
             }
-
-          /*  @Override
-            public void onDataChanged() {
-            }
-           @Override
-            protected void populateViewHolder(MenuViewHolder viewHolder, FoodItem model, int position) {
-                System.out.println("************tried to populate ***************");
-                System.out.println("Hello---"+model.getPicture()+"_________"+model.getCategory());
-
-                // Create a reference with an initial file path and name
-                String path = "images/"+model.getPicture();
-                StorageReference pathReference = storageReference.child(path);
-
-                Glide.with(RestaurantActivity.this)
-                        .using(new FirebaseImageLoader())
-                        .load(pathReference).centerCrop()
-                        .into(viewHolder.menuCategoryImage);
-                viewHolder.menuCategoryName.setText(model.getName());
-                viewHolder.menuCategoryName.setLongClickable(true);
-                viewHolder.menuCategoryName.setClickable(true);
-                final FoodItem selectedFoodItem = model;
-                viewHolder.setMenuItemOnClickListener(new MenuCategoryClickListener() {
-                  @Override
-                  public void onClick(View v, int posistion, boolean flag) {
-
-                      Toast.makeText(RestaurantActivity.this, selectedFoodItem.getCategory(),Toast.LENGTH_LONG).show();
-                  }
-
-                  @Override
-                  public void onLongClick(View v, int posistion, boolean flag) {
-
-                      Toast.makeText(RestaurantActivity.this, "delete",Toast.LENGTH_LONG).show();
-                      query.orderByChild("name")
-                              .equalTo(selectedFoodItem.getName())
-                              .addListenerForSingleValueEvent(new ValueEventListener() {
-                                  public void onDataChange(DataSnapshot dataSnapshot) {
-                                      if (dataSnapshot.hasChildren()) {
-                                          DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
-                                          firstChild.getRef().removeValue();
-                                      }
-                                  }
-
-                                  public void onCancelled(DatabaseError firebaseError) {
-                                  }
-                              });
-                  }
-                  }
-                );
-
-            }*/
-
         };
         categoryMenu.setAdapter(adapter);
 
